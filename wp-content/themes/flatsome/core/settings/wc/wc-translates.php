041cce8f65b1df4d0df769bdc58b7beee1b5c538
Minh-Home <hungquocminh1989@gmail.com>
2019-08-16 23:06:23 +0700

    update

--- wp-content/themes/flatsome/core/settings/wc/wc-translates.php
+++ wp-content/themes/flatsome/core/settings/wc/wc-translates.php
@@ -6,8 +6,8 @@
 */
 add_filter( 'gettext', 'translate_woocommerce_strings', 999, 3 );
 function translate_woocommerce_strings( $translated, $text, $domain ) {
-	$translated = str_ireplace( 'View cart', 'Xem Giỏ Hàng', $translated );
-	$translated = str_ireplace( 'Checkout', 'Thanh Toán', $translated );
+	//$translated = str_ireplace( 'View cart', 'Xem Giỏ Hàng', $translated );
+	//$translated = str_ireplace( 'Checkout', 'Thanh Toán', $translated );
 	
 	return $translated;
 }
