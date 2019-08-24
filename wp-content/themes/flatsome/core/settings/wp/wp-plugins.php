8a8105229be1bdf890505bd8dcd4d0208ac0c65f
Minh-Home <hungquocminh1989@gmail.com>
2019-08-25 00:33:41 +0700

    add plugins/fb-messenger-live-chat.zip

--- wp-content/themes/flatsome/core/settings/wp/wp-plugins.php
+++ wp-content/themes/flatsome/core/settings/wp/wp-plugins.php
@@ -22,6 +22,7 @@ add_action( 'after_setup_theme',  function(){
 	repoLoadPluginFromZip('wordpress-importer', 'wordpress-importer.php');
 	repoLoadPluginFromZip('premmerce-woocommerce-product-filter', 'premmerce-filter.php');
 	repoLoadPluginFromZip('quick-call-button', 'quick-call-button.php');
+	repoLoadPluginFromZip('fb-messenger-live-chat', 'fb-messenger-live-chat.php');
 	//repoLoadPluginFromStore();
 	
 });
