d9e0e4b298d886d84f5c9296f365babf82016aa8
Minh <hungquocminh1989@gmail.com>
2020-02-24 18:44:36 +0700

    Add chức năng export to excel

--- wp-content/themes/flatsome/core/settings/wp/wp-bulk-actions.php
+++ wp-content/themes/flatsome/core/settings/wp/wp-bulk-actions.php
@@ -15,9 +15,7 @@ add_action( 'admin_notices', 'my_bulk_action_admin_notice' );
  * Adds a new item into the Bulk Actions dropdown.
  */
 function register_my_bulk_actions( $bulk_actions ) {
-	$bulk_actions['to_page'] = 'Up To Facebook Page';
-	$bulk_actions['to_group'] = 'Up To Facebook Group';
-	$bulk_actions['to_profile'] = 'Up To Facebook Profile';
+	$bulk_actions['export_to_excel'] = 'Export to excel';
 	return $bulk_actions;
 }
 
@@ -25,13 +23,107 @@ function register_my_bulk_actions( $bulk_actions ) {
  * Handles the bulk action.
  */
 function my_bulk_action_handler( $redirect_to, $action, $post_ids ) {
-	if ( $action !== 'to_page' && $action !== 'to_group' && $action !== 'to_profile' ) {
+	if ( $action !== 'export_to_excel') {
 		return $redirect_to;
 	}
+	use PhpOffice\PhpSpreadsheet\Spreadsheet;
+	use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+	$arr_products = [];
+	$arr_products[] = ['No.', 'Product Name', 'Contents', 'Images'];
+	$arr_pages = [];
+	$arr_pages[] = ['No.', 'Token'];
+	
+	foreach ( $post_ids as $key => $post_id ) {
+		//repoPostToFacebook($post_id, $action);
+		//Get account token
+		$args['post_type'] = 'token';
+		$args['meta_key'] = 'fb_trang_thai';
+		$args['meta_value'] = 0;
+		$the_query = new WP_Query( $args );
+		if ( $the_query->have_posts() ) {
+		    while ( $the_query->have_posts() ) {
+		    	$the_query->the_post();
+				
+				$arr_token = explode("\r\n", get_field('fb_access_token_truy_cap_page'));
+				
+				foreach( $arr_token as $k => $token_page ) {
+					
+					$arr_pages[] = [$k, token_page];
+					
+					if($token_page != ''){
+						
+						//Get data product
+						$product = wc_get_product($id);
+						if($product != NULL && count($product) > 0){
+							
+							$gia_san_pham = $product->price;
+							$gia_san_pham_ctv = get_post_meta($product->id, 'ctv_price', true );
+							
+							//Get images
+							$attachment_ids = $product->get_gallery_attachment_ids();
+							foreach( $attachment_ids as $attachment_id ) {
+								$attachments[] = wp_get_attachment_image_src( $attachment_id, 'full' )[0];
+							}
+							$images_str = explode("\r\n", $attachments);
+							
+							$fb_tieu_de = get_field('fb_page_tieu_de', $product->id);
+							$fb_noi_dung_san_pham = get_field('fb_page_noi_dung_san_pham', $product->id);
+							$fb_thong_tin_bao_hanh = get_field('fb_page_thong_tin_bao_hanh', $product->id);
+							$fb_thong_tin_lien_he = get_field('fb_page_thong_tin_lien_he', $product->id);
+							$fb_thong_tin_lien_ket = get_field('fb_page_thong_tin_lien_ket', $product->id);
+							$main_page_content = "
+								$fb_tieu_de
+								$fb_noi_dung_san_pham
+								$fb_thong_tin_bao_hanh
+								Giá bán : $gia_san_pham
+								$fb_thong_tin_lien_he
+								$fb_thong_tin_lien_ket
+							";
 
-	foreach ( $post_ids as $post_id ) {
-		repoPostToFacebook($post_id, $action);
+							$arr_products[]	= [$key + 1, '', $main_page_content, $images_str];			
+							
+						}
+						
+					}
+				}
+		    }
+		}
+		wp_reset_postdata();
 	}
+	
+	//Sheet Products
+	$spreadsheet = new Spreadsheet();
+	$sheet_product = $spreadsheet->getActiveSheet();
+	$sheet_product->setTitle('Products');
+	$sheet_product->fromArray(
+		$arr_products,  // The data to set
+		NULL,        // Array values with this value will not be set
+		'A1'         // Top left coordinate of the worksheet range where
+					 //    we want to set these values (default is A1)
+	);
+	
+	//Sheet Pages
+	$sheet_page = $spreadsheet->createSheet();
+	$sheet_page->setTitle('Pages');
+	$sheet_page->fromArray(
+		$arr_pages,  // The data to set
+		NULL,        // Array values with this value will not be set
+		'A1'         // Top left coordinate of the worksheet range where
+					 //    we want to set these values (default is A1)
+	);
+	
+	//Format header excel
+	$styleArray = [
+		'font' => [
+			'bold' => true,
+		],
+	];
+	$sheet_product->getStyle('A1:D1')->applyFromArray($styleArray);
+	$sheet_page->getStyle('A1:B1')->applyFromArray($styleArray);
+	
+	//Save file
+	$writer = new Xlsx($spreadsheet);
+	$writer->save('wp-content/themes/flatsome/core/test.xlsx');
 
 	$redirect_to = add_query_arg( 'bulk_reposts', count( $post_ids ), $redirect_to );
 
