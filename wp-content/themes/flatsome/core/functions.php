9c383198fa6b67946a892cab5cdef558647129bb
Minh-Home <hungquocminh1989@gmail.com>
2020-04-02 19:33:49 +0700

    update

--- wp-content/themes/flatsome/core/functions.php
+++ wp-content/themes/flatsome/core/functions.php
@@ -49,8 +49,10 @@ function repoDebugVar($object){
 | CHANGE MY CURRENCY
 |-------------------------------------------------------------------------- 
 */
-add_filter( 'woocommerce_currencies', 'add_my_currency' ); 
-function add_my_currency( $currencies ) {
-     $currencies['vnd'] = __( ' VNĐ', 'woocommerce' );
-     return $currencies;
+add_filter('woocommerce_currency_symbol', 'add_my_currency_symbol', 10, 2);
+function add_my_currency_symbol( $currency_symbol, $currency ) {
+	switch( $currency ) {
+		case 'vnd': $currency_symbol = ' VNĐ'; break;
+	}
+	return $currency_symbol;
 }
