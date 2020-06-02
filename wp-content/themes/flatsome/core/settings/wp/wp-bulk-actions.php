b246eb6e98e97b46aee45089433c1368efe505c5
Minh-Home <hungquocminh1989@gmail.com>
2020-06-03 01:03:40 +0700

    update

--- wp-content/themes/flatsome/core/settings/wp/wp-bulk-actions.php
+++ wp-content/themes/flatsome/core/settings/wp/wp-bulk-actions.php
@@ -72,8 +72,9 @@ function my_bulk_action_handler( $redirect_to, $action, $post_ids ) {
 							],
 				'headers' => [
 								'Content-Type' => 'application/json',
-								'Authorization'=>"Bearer 1a2e53f2db298e7a0dc0c71d870aa5783bd7dc74"
+								'Authorization' => "Bearer 1a2e53f2db298e7a0dc0c71d870aa5783bd7dc74"
 							],
+				'sslverify'   => false,
 			]
 		);
 		echo "<pre>";
