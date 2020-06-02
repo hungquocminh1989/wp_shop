5e107580f31182433437a8413ea4d29041ca6b3b
Minh-Home <hungquocminh1989@gmail.com>
2020-06-03 00:50:17 +0700

    update

--- wp-content/themes/flatsome/core/settings/wp/wp-bulk-actions.php
+++ wp-content/themes/flatsome/core/settings/wp/wp-bulk-actions.php
@@ -63,7 +63,22 @@ function my_bulk_action_handler( $redirect_to, $action, $post_ids ) {
 	foreach ( $post_ids as $key => $post_id ) {
 		//Get data product
 		$product = wc_get_product($post_id);
-		$product_url = 'koolwatch.me';//get_permalink( $post_id ) ;
+		$product_url = get_permalink( $post_id ) ;
+		$post_request = wp_remote_post(
+			'https://api-ssl.bitly.com/v4/bitlinks', 
+			[
+				'body' => 	[
+								'long_url'=>$product_url
+							],
+				'headers' => [
+								'Authorization'=>"Bearer 1a2e53f2db298e7a0dc0c71d870aa5783bd7dc74"
+							],
+			]
+		);
+		echo 111;
+		var_dump($post_request);
+		die();
+		$bitly_url = '';
 		
 		if($product != NULL){
 			$product_name = trim($product->get_title());
