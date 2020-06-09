193de04b7888a1480b647aa577516f9bae88fdba
Minh <hungquocminh1989@gmail.com>
2020-06-09 18:16:11 +0700

    UPdate

--- wp-content/themes/flatsome/core/settings/wp/wp-ajax.php
+++ wp-content/themes/flatsome/core/settings/wp/wp-ajax.php
@@ -92,7 +92,7 @@ function repo_action_get_products() {
 			$log_path = WP_CONTENT_DIR . "/download/$log_filename";
 			
 			//Execute shell script upload to facebook
-			$command = "sh $path_shell $path_python $token $log_path";
+			$command = "sh $path_shell $path_python $log_path $token";
 			$output = exec($command);
 			
 			wp_send_json_success("OK");
@@ -100,5 +100,42 @@ function repo_action_get_products() {
 		else{
 			wp_send_json_error("Token error.");
 		}
+	}
+}
+
+/*
+|--------------------------------------------------------------------------
+| AJAX GET LONG TOKEN
+|-------------------------------------------------------------------------- 
+*/
+add_action( 'wp_ajax_get_long_token', 'repo_action_get_long_token' );//Khai báo khi sử dụng bên Admin
+//add_action( 'wp_ajax_nopriv_get_token', 'repo_action_get_products' );//Khai báo khi sử dụng bên Public
+function repo_action_get_long_token() {
+	
+	if(isset($_POST['short_token']) == TRUE
+		&& $_POST['short_token'] != ''
+		&& isset($_POST['page_id_string']) == TRUE
+		&& $_POST['page_id_string'] != ''
+	){
+		$token = $_POST['short_token'];
+		$page_id_string = $_POST['page_id_string'];
+		
+		$path_shell = RUN_PYTHON_SHELL_SCRITP;
+		$path_python = get_template_directory() . "/linux_shell_script/GenerateToken.py";
+		
+		$ymdhis = Date('YmdHis');
+		$log_filename = 'Result_get_long_token_' . $ymdhis . '.log';
+		$log_path = WP_CONTENT_DIR . "/download/$log_filename";
+		
+		//Execute shell script
+		$command = "sh $path_shell $path_python $log_path $token $page_id_string";
+		$output = exec($command);
+		
+		wp_send_json_success($output);
+		
+		
+	}
+	else{
+		wp_send_json_error("Nhập chưa đủ thông tin.");
 	}
 }
