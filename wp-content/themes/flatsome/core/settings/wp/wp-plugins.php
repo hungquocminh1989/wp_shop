a3d8e6d0ad9b4bb9fbe68415a1b159ccf4bf38a1
Minh <hungquocminh1989@gmail.com>
2020-06-15 18:03:26 +0700

    add plugins/wp-smush-pro.zip

--- wp-content/themes/flatsome/core/settings/wp/wp-plugins.php
+++ wp-content/themes/flatsome/core/settings/wp/wp-plugins.php
@@ -33,7 +33,7 @@ add_action( 'after_setup_theme',  function(){
 	repoLoadPluginFromZip('pagination-styler-for-woocommerce', 'pagination-styler.php');
 	repoLoadPluginFromZip('w3-total-cache', 'w3-total-cache.php');
 	repoLoadPluginFromZip('autoptimize', 'autoptimize.php');
-	repoLoadPluginFromZip('wp-smushit', 'wp-smush.php');
+	repoLoadPluginFromZip('wp-smush-pro', 'wp-smush.php');
 	//repoLoadPluginFromStore();
 	
 });
